1ac70bcd5957d92ee9630527b3f510e54aceebad
Great Amubode <gamubode@gmail.com>
2022-02-23 10:47:40 +0100

    The background colour of the icon is pink so that it will show against the white background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import HamburgerMenuIcon from "./components/atoms/HamburgerMenuIcon";
+import NotificationBell from "./components/atoms/NotificationBell";
+
 function App() {
   return (
     <div className="App">
       <h1>Ship Secure</h1>
+      <HamburgerMenuIcon />
+      <NotificationBell />
     </div>
   );
 }
