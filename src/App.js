accb91a5bf1dbde1356ae2b2c53e15157ec6eade
promiseNwafor <promisenwafor955@gmail.com>
2022-02-24 11:52:03 +0100

    add dropdown menu input update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./utils/theme";
-import DropdownMenu from "./components/atoms/DropdownMenu";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <h1>Ship Secure</h1>
-        <DropdownMenu labelText="Age" items={[{ value: 2, child: "One" }]} />
       </div>
     </ThemeProvider>
   );
