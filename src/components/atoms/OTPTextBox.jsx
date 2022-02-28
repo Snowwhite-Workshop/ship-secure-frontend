17b28a2e24cddd76e4c70b7a1d33d19f9f3259d5
Chukwu Chiedozie <79095890+chuksdozie@users.noreply.github.com>
2022-02-28 10:23:14 +0100

    Revert "Hamburger Menu Icon and Notification Bell Icon"

--- src/components/atoms/OTPTextBox.jsx
+++
@@ -1,8 +0,0 @@
-import TextField from "@mui/material/TextField";
-import "./style.css";
-
-function OTPTextBox() {
-  return <TextField sx={{ width: 100 }} variant="outlined" />;
-}
-
-export default OTPTextBox;
