2329180da5b412726cbeb5c2d9bfd25d32962688
Adeboga Abigail <bogadeji@gmail.com>
2022-02-28 15:32:02 +0100

    Use Avatar tag instead of img

--- src/components/atoms/UserAvatar.js
+++ src/components/atoms/UserAvatar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
 
 function UserAvatar({ 
     size,
@@ -8,15 +9,22 @@ function UserAvatar({
 }) {
     return (
         <Box
-        sx={{
-            width: size,
-            height: size,
-            margin: margin,
-            borderRadius: '50%',
-            background: `url('${imgUrl}')`,
-            backgroundColor: ' #C4C4C4;'
-        }}
-                >
+            sx={{
+                width: size,
+                height: size,
+                margin: margin,
+                borderRadius: '50%',
+                backgroundColor: ' #C4C4C4;',
+                objectFit: 'contain'
+            }}
+        >
+            {imgUrl && <Avatar src={imgUrl} alt="User Avatar"
+                sx={{
+                    width: size,
+                    maxWidth: size,
+                    height: size
+                }}
+            />}
                     
         </Box>
     );
