0e0e77fd011adca46b3c7ca25c07d15d2c45aa46
Adeboga Abigail <bogadeji@gmail.com>
2022-02-28 23:00:26 +0100

    Create app logo component/atom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import HamburgerMenuIcon from "./components/atoms/HamburgerMenuIcon";
-import NotificationBell from "./components/atoms/NotificationBell";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Ship Secure</h1>
-      <HamburgerMenuIcon />
-      <NotificationBell />
-    </div>
-=======
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./utils/theme";
 
@@ -21,7 +8,6 @@ function App() {
         <h1>Ship Secure</h1>
       </div>
     </ThemeProvider>
->>>>>>> upload
   );
 }
 
---
+++ src/components/atoms/Logo.js
@@ -0,0 +1,25 @@
+import Box from "@mui/material/Box";
+
+function Logo({dark, light, size}) {
+     return (
+         <Box
+            sx={{
+                width: size,
+                background: 'grey'
+            }} 
+        >
+            { dark && <img src="assets/Logo-dark.svg" alt="Logo"
+                
+            />}
+            { light && <img src="assets/Logo-light.svg" alt="Logo"
+                
+                />}
+         </Box>
+     )
+}
+
+Logo.defaultProps = {
+    size: '200px'
+}
+
+export default Logo;
