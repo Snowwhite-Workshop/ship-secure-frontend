051b68a6bbc2a46745b09bc41202c89ad505e5bf
Adeboga Abigail <bogadeji@gmail.com>
2022-03-02 19:37:43 +0100

    Delete code added into atoms/style.css and index.js files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Buttons from './components/atoms/Buttons';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Buttons />
   </React.StrictMode>,
   document.getElementById('root')
 );
