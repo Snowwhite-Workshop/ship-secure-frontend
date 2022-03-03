cc26bcb55894ac1c2a2eea5d52c94c3d822a2998
chuksdozie <chuksdozie48@gmail.com>
2022-03-03 13:48:56 +0100

    repairs to the broken  branch

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
-import React from "react";
-import HamburgerMenuIcon from "./components/atoms/HamburgerMenuIcon";
-import NotificationBell from "./components/atoms/NotificationBell";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Ship Secure</h1>
-      <HamburgerMenuIcon />
-      <NotificationBell />
-    </div>
-=======
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./utils/theme";
+import "./index";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <h1>Ship Secure</h1>
+        <h3 style={{ color: "red" }}>
+          Don't Push App.js in your PR's (for now)
+        </h3>
       </div>
     </ThemeProvider>
->>>>>>> upload
   );
 }
 
