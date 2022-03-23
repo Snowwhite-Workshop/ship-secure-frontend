25db1a399a3fbe2b9361b5329211bce46e11bb69
chuksdozie <chuksdozie48@gmail.com>
2022-03-23 14:25:36 +0100

    default props and props adjustment to regular text

--- src/components/atoms/RegularText.js
+++ src/components/atoms/RegularText.js
@@ -2,9 +2,15 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 
 export default function RegularText(props) {
-  const { color = "#000000", size = "14px", text, weight = "normal" } = props;
+  const { color, size, text, weight, align, margin } = props;
   return (
-    <Typography color={color} fontSize={size} fontWeight={weight}>
+    <Typography
+      color={color || "#000000"}
+      fontSize={size || "14px"}
+      fontWeight={weight || "normal"}
+      textAlign={align || "left"}
+      margin={margin}
+    >
       {text}
     </Typography>
   );
