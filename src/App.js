0df9a9bf0b1e04bd2599464642b92e731d48ae4c
Great Amubode <gamubode@gmail.com>
2022-03-29 13:24:18 +0100

    Error 404

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./utils/theme";
 import "./index";
-import Error400 from "./pages/Error400";
+import Error404 from "./pages/Error404";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Error400 />
+        <Error404 />
       </div>
     </ThemeProvider>
   );
